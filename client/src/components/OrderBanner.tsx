5b1f938bbe29c19c3b9a16a1aebb64e4a93766b1
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-23 00:43:02 +0300

    orderbanner commit

--- client/src/components/OrderBanner.tsx
+++ client/src/components/OrderBanner.tsx
@@ -7,9 +7,90 @@ function OrderBanner() {
       sx={{
         width: "1266px",
         height: "386px",
+        background: "#2F2F2F",
+        borderRadius: "40px",
+        flexShrink: 0,
       }}
     >
       <Box
+        sx={{
+          paddingLeft: "40px",
+          paddingTop: "35px",
+        }}
+      >
+        <Typography
+          sx={{
+            width: "581.742px",
+            color: "#FFF",
+            fontFamily: "Roboto",
+            fontSize: "45px",
+            fontStyle: "normal",
+            fontWeight: 700,
+            lineHeight: "95.688%", // 43.059px
+            letterSpacing: "1.35px",
+          }}
+        >
+          Make Your First Order and Get
+        </Typography>
+        <Typography
+          sx={{
+            width: "207.5px",
+            color: "#FF9921",
+            fontFamily: "Roboto",
+            fontSize: "45px",
+            fontStyle: "normal",
+            fontWeight: 700,
+            lineHeight: "95.688%", // 43.059px
+            letterSpacing: "1.35px",
+          }}
+        >
+          50% Off
+        </Typography>
+        <Typography
+          sx={{
+            width: "590.388px",
+            height: "61px",
+            flexShrink: 0,
+            color: "#FFF",
+            fontFamily: "Roboto",
+            fontSize: "16px",
+            fontStyle: "normal",
+            fontWeight: 400,
+            lineHeight: "144.687%", // 23.15px
+            letterSpacing: "0.48px",
+            opacity: 0.9,
+          }}
+        >
+          In publishing and graphic design, Lorem ipsum is a placeholder text
+          commonly used to demonstrate the visual form of a document or a
+          typeface without.
+        </Typography>
+
+        <Button
+          sx={{
+            width: "248.26px",
+            height: "60px",
+            flexShrink: 0,
+          }}
+        >
+          <Typography
+            sx={{
+              width: "149.45px",
+              color: "#FFF",
+              fontFamily: "Roboto",
+              fontSize: "24px",
+              fontStyle: "normal",
+              fontWeight: 700,
+              lineHeight: "144.687%", // 34.725px
+              letterSpacing: "0.72px",
+            }}
+          >
+            Order Now
+          </Typography>
+        </Button>
+      </Box>
+      <Box></Box>
+      {/* <Box
         sx={{
           width: "1266px",
           height: "386px",
@@ -113,7 +194,7 @@ function OrderBanner() {
         >
           Order Now
         </Typography>
-      </Button>
+      </Button> */}
     </Box>
   );
 }
