831e7895b38798ae1390b3acd5a5f922d2c6304e
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-22 20:49:39 +0300

    added navbar component

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <Box>
+      <Typography>nav bar</Typography>
+    </Box>
+  )
+}
+
+export default NavBar
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import {  Box, Typography } from "@mui/material";
+import NavBar from "../components/NavBar";
+
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography>Home</Typography>
+      <NavBar/>
+    </Box>
+  )
+}
+
+export default Home
