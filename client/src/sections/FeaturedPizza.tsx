ecc41234457ca741ecfc669d5144dda997d57324
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-22 23:52:16 +0300

    featured pizza commit

--- client/src/sections/FeaturedPizza.tsx
+++ client/src/sections/FeaturedPizza.tsx
@@ -4,15 +4,39 @@ function FeaturedPizza() {
   return (
     <Box
       sx={{
-        display: 'flex',
-        padding: '65px 87px 110px 87px',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'stretch',
-        background: '#FFF8F1',
+        display: "flex",
+        padding: "65px 87px 110px 87px",
+        justifyContent: "center",
+        alignItems: "center",
+        alignSelf: "stretch",
+        background: "#FFF8F1",
       }}
     >
       {/* Your content here */}
+      <Box
+        sx={{
+          display: "flex",
+          width: "1266px",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          gap: "25px",
+        }}
+      >
+        {/* Inner content here */}
+        <Typography
+          sx={{
+            alignSelf: "stretch",
+            color: "rgba(0, 0, 0, 0.50)",
+            fontFamily: "Inter",
+            fontSize: "50px",
+            fontStyle: "normal",
+            fontWeight: 500,
+            lineHeight: "150%", // 75px
+          }}
+        >
+          Featured pizza
+        </Typography>
+      </Box>
     </Box>
   );
 }
