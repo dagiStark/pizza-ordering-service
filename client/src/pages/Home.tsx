b00e24af8edbb702ab7ec16589a1a8034bebfe9a
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-23 23:26:26 +0300

    completed fasting section skeleton

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -16,7 +16,7 @@ const Home = () => {
         <FeaturedPizza />
         <TopRestaurants />
         <PopularPizzas />
-        {/* <Fasting /> */}
+        <Fasting />
         {/* <ContactUs />  */}
       </Stack>
     </Box>
