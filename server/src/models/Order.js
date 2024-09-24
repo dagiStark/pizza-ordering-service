167ad80d289e2a4291f2a7b26277110f71d3b1f7
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-24 19:40:04 +0300

    added order model

---
+++ server/src/models/Order.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Order = sequelize.define('Order', {
+    status: { type: DataTypes.ENUM('Preparing', 'Delivered'), defaultValue: 'Preparing' },
+    customerId: { type: DataTypes.INTEGER },
+    pizzaId: { type: DataTypes.INTEGER },
+    restaurantId: { type: DataTypes.INTEGER },
+  });
+
+  Order.associate = (models) => {
+    Order.belongsTo(models.User, { foreignKey: 'customerId' });
+    Order.belongsTo(models.Pizza, { foreignKey: 'pizzaId' });
+    Order.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' });
+  };
+
+  return Order;
+};
