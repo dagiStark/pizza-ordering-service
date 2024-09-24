cc9b3e564d35606cc506c3ce87ce38c7e70caa96
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-24 19:24:32 +0300

    added user model

---
+++ server/src/models/User.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    name: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING }, // ['customer', 'restaurant-manager', 'super-admin']
+    restaurantId: { type: DataTypes.INTEGER, allowNull: true }, // For restaurant users
+  });
+
+  User.associate = (models) => {
+    User.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' });
+    User.hasMany(models.Order, { foreignKey: 'customerId' });
+  };
+
+  return User;
+};
