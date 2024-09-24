438688b69d8c4377b7cdd986b1774e80ff61b943
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-24 19:26:09 +0300

    added restaurant model

---
+++ server/src/models/Restaurant.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Restaurant = sequelize.define('Restaurant', {
+    name: { type: DataTypes.STRING },
+  });
+
+  Restaurant.associate = (models) => {
+    Restaurant.hasMany(models.Pizza);
+    Restaurant.hasMany(models.Topping);
+    Restaurant.hasMany(models.User);
+  };
+
+  return Restaurant;
+};
