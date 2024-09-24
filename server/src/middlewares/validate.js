7c5db87837c412c480acb9ae8bf2012236752eee
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-24 20:47:58 +0300

    added validation middleware

---
+++ server/src/middlewares/validate.js
@@ -0,0 +1,10 @@
+const validate = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.body);
+    next();
+  } catch (error) {
+    return res.status(400).json(error.errors);
+  }
+};
+
+module.exports = { validate };
