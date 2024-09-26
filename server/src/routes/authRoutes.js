c1411fd563ab92bd214f36d6ef7979d4800e126b
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-26 09:59:28 +0300

    added restaurant registration validator

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -6,11 +6,11 @@ const {
   register,
 } = require("../controllers/authControllers.js");
 const { validate } = require("../middlewares/validate");
-const { registerSchema } = require("../utils/validators");
+const { registerSchema, restaurantSchema } = require("../utils/validators");
 
 const router = express.Router();
 
-router.post("/register", validate(registerSchema), register);
+router.post("/register", validate(restaurantSchema), register);
 router.post("/sign-up", signup);
 router.post("/login", login);
 router.post("/logout", logout);
--- server/src/utils/validators.js
+++ server/src/utils/validators.js
@@ -7,6 +7,16 @@ const registerSchema = z.object({
   role: z.enum(["customer", "restaurant-manager", "super-admin"]),
 });
 
+const restaurantSchema = z.object({
+  id: z.number().int().positive().optional(), // Auto-incremented in the DB, so can be optional for validation
+  name: z.string().min(1, { message: "Name is required" }), // Must not be empty
+  location: z.string().optional(), // Can be null or optional
+  superAdmin: z.string().min(1, { message: "superAdmin is required" }), // Cannot be empty
+  password: z
+    .string()
+    .min(4, { message: "Password must be at least 4 characters long" }), // Minimum 8 characters for the password
+});
+
 const pizzaSchema = z.object({
   name: z.string(),
   defaultToppings: z.array(z.number()), // Array of Topping IDs
@@ -19,4 +29,4 @@ const orderSchema = z.object({
   restaurantId: z.number(),
 });
 
-module.exports = { registerSchema, pizzaSchema, orderSchema };
+module.exports = { registerSchema, pizzaSchema, orderSchema, restaurantSchema };
