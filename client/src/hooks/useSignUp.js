1eb5ec8f5bcc0e54e3514d0255ccbd41a91f0b29
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-26 15:48:47 +0300

    added signup hook logic

---
+++ client/src/hooks/useSignUp.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
+
+const useSignUp = () => {
+  const [loading, setLoading] = useState(false);
+  const {setAuthUser} = useAuthContext();
+
+  const signUp = async ({
+    fullName,
+    email,
+    password,
+    confirmPassword,
+    restaurantId,
+  }) => {
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return false;
+    }
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/sign-up", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          fullName,
+          email,
+          password,
+          confirmPassword,
+          restaurantId,
+        }),
+      });
+
+      const data = await res.json();
+      if(data.error){
+        throw new Error(data.error)
+      }
+      localStorage.setItem("chat-user", JSON.stringify(data))
+      setAuthUser(data)
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {loading, signUp};
+};
+
+export default useSignUp;
