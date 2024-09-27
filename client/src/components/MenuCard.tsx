d60076ef56eb74ce4e4eef101424c81b449ed4ff
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-27 10:36:23 +0300

    added file uploader

--- client/src/components/MenuCard.tsx
+++ client/src/components/MenuCard.tsx
@@ -7,18 +7,16 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import UploadIcon from "@mui/icons-material/Upload";
+import { styled } from "@mui/material/styles";
 
 const MenuCard = () => {
   return (
     <Box>
       <Box>
-        <Typography>Menu</Typography>
-        <TextField
-          fullWidth
-          label="Email address"
-          type="email"
-          margin="normal"
-        />
+        <Typography>Add Menu</Typography>
+        <TextField fullWidth label="Name" type="text" margin="normal" />
 
         <Box>
           <Typography>Topping</Typography>
@@ -29,18 +27,42 @@ const MenuCard = () => {
             <FormControlLabel control={<Checkbox />} label="Onions" />
             <FormControlLabel control={<Checkbox />} label="Olives" />
           </Stack>
+          <Box>
+            <AddIcon />
+            <Button variant="contained">Add</Button>
+          </Box>
         </Box>
-        <TextField
-          fullWidth
-          label="Email address"
-          type="email"
-          margin="normal"
-        />
-        <Button variant="outlined">Outlined</Button>
-        <Button variant="contained">Contained</Button>
+        <TextField fullWidth label="Price" type="text" margin="normal" />
+        <Button
+          component="label"
+          role={undefined}
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<UploadIcon />}
+        >
+          Upload Pizza Photo
+          <VisuallyHiddenInput
+            type="file"
+            onChange={(event) => console.log(event.target.files)}
+            multiple
+          />
+        </Button>
+        <Button variant="contained">Submit</Button>
       </Box>
     </Box>
   );
 };
 
 export default MenuCard;
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
