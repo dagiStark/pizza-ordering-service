975f5c020952502598046dc9be4f084ab00d32d8
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-09-27 20:57:27 +0300

    added image column to upload pizza image

--- server/src/models/Pizza.js
+++ server/src/models/Pizza.js
@@ -1,14 +1,16 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const Pizza = sequelize.define('Pizza', {
+  const Pizza = sequelize.define("Pizza", {
     name: { type: DataTypes.STRING },
     restaurantId: { type: DataTypes.INTEGER },
+    price: { type: DataTypes.FLOAT },
+    image: { type: DataTypes.BLOB("long") }, // Store image as BLOB
   });
 
   Pizza.associate = (models) => {
-    Pizza.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' });
-    Pizza.hasMany(models.Topping, { as: 'defaultToppings' });
+    Pizza.belongsTo(models.Restaurant, { foreignKey: "restaurantId" });
+    Pizza.hasMany(models.Topping, { as: "defaultToppings" });
   };
 
   return Pizza;
