a44377c7e3b12e610bfd1acc5db82623f7daae37
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-04 09:18:08 +0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://pizza-ordering-service-api.vercel.app/", // Your backend server
+        target: "https://pizza-ordering-service-api.vercel.app", // Your backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), // Optional, if you want to remove `/api` from the request path
       },
