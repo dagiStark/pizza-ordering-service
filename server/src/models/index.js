1c34dcd75bab78f695cc02a80660213bc27b076c
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-09 23:06:13 +0300

    Update index.js

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -10,6 +10,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432, // Include port
     dialect: "postgres",
+    dialectModule: require("pg"),
     dialectOptions: {
       ssl: {
         require: true, // Supabase requires SSL
